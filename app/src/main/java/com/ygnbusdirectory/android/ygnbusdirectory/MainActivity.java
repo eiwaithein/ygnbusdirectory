5ab1624d4811ba118729904d809dda9499b6e922
Ei Wai Wai Thein <eiwaiwai.thein@sg.tribalworldwide.com>
2017-02-11 23:43:01 +0800

    first version of app.

--- app/src/main/java/com/ygnbusdirectory/android/ygnbusdirectory/MainActivity.java
+++ app/src/main/java/com/ygnbusdirectory/android/ygnbusdirectory/MainActivity.java
@@ -1,13 +1,92 @@
 package com.ygnbusdirectory.android.ygnbusdirectory;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private WebView webView;
+    private static String WEB_VIEW_URL = "http://www.ygnbusdirectory.com/";
+    private ProgressBar mPbar = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webView);
+        mPbar = (ProgressBar)findViewById(R.id.progressBar);
+
+        webView.setWebViewClient(new MyBrowser());
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        //webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        webView.loadUrl(WEB_VIEW_URL);
+        webView.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);
+
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        webView.loadUrl(WEB_VIEW_URL);
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            switch (keyCode) {
+                case KeyEvent.KEYCODE_BACK:
+                    if (webView.canGoBack()) {
+                        webView.goBack();
+                    } else {
+                        finish();
+                    }
+                    return true;
+            }
+
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+
+
+
+
+    private class MyBrowser extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            mPbar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            mPbar.setVisibility(View.GONE);
+        }
+    }
+
 }
